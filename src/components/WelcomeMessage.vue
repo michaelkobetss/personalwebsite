c9b25c72a57653b621dbf65a7fd60340ade741a6
michaelkobetss <michael.kobetss@gmail.com>
2023-11-12 11:31:10 +0200

    sroll optimisation

--- src/components/WelcomeMessage.vue
+++ src/components/WelcomeMessage.vue
@@ -37,14 +37,27 @@ export default {
     };
   },
 
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+
+ 
+
   methods: {
     scrollDown() {
-      this.isArrowClicked = !this.isArrowClicked;
       window.scrollTo({
         top: window.innerHeight,
         behavior: 'smooth'
       });
-    }
+    },
+    
+    handleScroll() {
+      console.log("handlescroll worked")
+      if (window.scrollY) {
+        this.isArrowClicked = true;
+        window.removeEventListener('scroll', this.handleScroll);
+      } 
+    },
   }
 };
 </script>
